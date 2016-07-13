32d96ab08efdbd16c120cc2feaa0ff4e3cd517e4
Eyassu Getachew <Eyassu Getachew>
2016-07-13 23:03:27 +0300

    Added endpoint definitions for the REST API

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/airlines', function (req, res, next) {
+  res.end('Lists available airlines from the Flight API.');
+});
+
+router.get('/airports', function (req, res, next) {
+  res.end('Lists all matching airports from the Flight API.');
+});
+
+router.get('/search', function (req, res, next) {
+  res.end('Searches for and lists all matching flights from the Flight API');
+});
+
 module.exports = router;
